b399621fc802895762dde4f9af13c16e1d09df8a
mcianci2020 <marcos.cianci@gmail.com>
2022-11-10 01:16:04 -0300

    update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,25 @@
 # NTTDATA - DIGITAL ARCHITECTURE
 # Create: Marcos Cianci - mlopesci@emeal.nttdata.com
 
+
+### Global Variables ###
 variable region {
   type        = string
   default     = ""
   description = "AWS Region Account"
 }
+
+
+### VPC ###
+
+variable "cidr_blocks" {
+
+  description = "The CIDR Blocks the VPC."
+  type        = string
+  
+}
+
+### Tags ###
+
+variable "tags" {}
+variable "tags_vpc" {}

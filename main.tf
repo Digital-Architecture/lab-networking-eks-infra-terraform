486420dd3bf5848c74d6803982284cd240d51b0f
mcianci2020 <marcos.cianci@gmail.com>
2022-11-12 00:42:42 -0300

    Create AWS Internet Gateway for VPC

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 ### VPC ###
 module "vpc-eks" {
 
-    source                          = "git::https://github.com/Digital-Architecture/terraform-modules-aws-networking.git//vpc?ref=v.0.0.5"
+    source                          = "git::https://github.com/Digital-Architecture/terraform-modules-aws-networking.git//vpc?ref=v.0.0.6"
 
     cidr_block                      = var.cidr_block
     instance_tenancy                = "default"
@@ -16,4 +16,15 @@ module "vpc-eks" {
     enable_ipv6                     = false 
     tags                            = var.tags
     tags_vpc                        = var.tags_vpc
+}
+
+### Internet Gateway ###
+
+module "internet-gateway" {
+
+    source                  = "git::https://github.com/Digital-Architecture/terraform-modules-aws-networking.git//internet_gateway?ref=v.0.0.6"
+
+    vpc_id                  = module.vpc-eks.vpc-id
+    name_internet_gateway   = "internetGateway-eks-lab"
+    tags                    = var.tags
 }
